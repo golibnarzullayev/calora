aefc6737ea07e7850143df83afa14de69cae645e
golibnarzullayev <gnarzullayev2000@gmail.com>
2026-03-01 19:10:03 +0500

    Fully completed calora app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://calora-api.gignite.uz",
         changeOrigin: true,
       },
     },
-    allowedHosts: ["grow-effect-poem-procedure.trycloudflare.com"],
+    allowedHosts: ["calora.gignite.uz"],
     headers: {
       "Service-Worker-Allowed": "/",
     },
